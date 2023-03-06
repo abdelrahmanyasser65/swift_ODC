8b3714b387b2b6104198b37835c2447e564f6c37
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-03-06 19:56:53 +0200

    edit

--- inrto.swift
+++ inrto.swift
@@ -1,3 +1,50 @@
-print("hello,world")
-print("new")
-print("hewllsodl")
+print("Hello,World")
+var num=10
+print(num)
+var fName:String
+fName="Abdo"
+var lName:String="yasser"
+print(fName+lName)
+print(fName,lName)
+//constants
+let a = 5
+// a=6 error
+let pi: Float = 3.14
+let pass:Bool = true
+let char : Character = "s"
+var salary : Double = 24.6
+
+var str1="Hello,world"
+var str2="new str"
+print(str1==str2)
+str1.append(str2)
+print(str1)
+print(str1.count)
+
+var number=10
+print("the number is \(number)")
+
+// multiline string 
+var str: String = """
+Swift is awesome
+I love Swift
+"""
+print(str)
+
+print("Good morning",terminator:" ")
+print("it's rainy")
+//Good morning it's rainy
+ 
+ print("new year",2022,"hello",sparator:"..")
+
+
+ //input
+
+ print("Enter your favorite programming language:")
+let name = readLine()
+
+print("Your favorite programming language is \(name!).")
+
+let name=readLine()
+
+
---
+++ operators.swift
@@ -0,0 +1,45 @@
+
+
+print(5+6)
+//arithmetic
+var a=6
+var b=12
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+print(a%b)
+print(b%a)
+print(9/4)
+print(9%4)
+print(a+=1)
+//comparison operators
+print(a>b)
+print(a != b)
+print(a==b)
+print(a<=b)
+//logical operators
+print((a>2)&&(b>15))
+print((a>4)||(b<4))
+print(!false)  
+//Ternary Conditional Operator
+
+let marks=60
+let result=(marks >= 40) ? "pass":"fail"
+print("you ",result,"the exam")
+
+if(marks>70){
+    print("pass")
+}
+else{
+    print("fail")
+}
+marks == 0 ? print("Zero") : 
+marks >= 50 ? print("pass") : print("fail")
+//binary 
+var a = 12
+var b = 25
+var res = a & b //and
+print(res)
+print(a | b)  //or
+print(a ^ b)//Xor
